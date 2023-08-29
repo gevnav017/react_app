db306e03c13378f7d71bed73df9eebf0491d6cbc
Gevorg Navasardyan <gevorgnavasardyan@Gevorgs-MacBook-Pro.local>
2023-08-29 14:59:36 -0700

    new additions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import {puppyList} from './data.js'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-console.log('test')
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
       <div>
-hi
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
