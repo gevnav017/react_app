cc6c19a5598d30cbb82cc0635c02efc6dba21f12
Gevorg Navasardyan <gevorgnavasardyan@Gevorgs-MacBook-Pro.local>
2023-09-28 18:07:46 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ import { useState } from 'react'
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
+  
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p onClick={() => {
-              setFeatPupId(puppy.id)
-            }} key={puppy.id}> {puppy.name} </p>
-          })
+            const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+            console.log(featuredPup)
+            return (
+              <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}> 
+                {puppy.name} 
+              </p>
+            )
+          })        
         }
-
-        <div>
-          { featPupId && <p>{ featPupId }</p> }
-        </div>
       </div>
     </>
   )
